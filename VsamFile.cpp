6ecb344cf141be1ab9f9a3877f25e8c57c7f3f88
John Barboza <jbarboza@ca.ibm.com>
2017-12-05 13:06:11 -0500

    add copyright statement

--- VsamFile.cpp
+++ VsamFile.cpp
@@ -1,3 +1,9 @@
+/*
+ * Licensed Materials - Property of IBM
+ * (C) Copyright IBM Corp. 2017. All Rights Reserved.
+ * US Government Users Restricted Rights - Use, duplication or disclosure restricted by GSA ADP Schedule Contract with IBM Corp.
+*/
+
 #include "VsamFile.h"
 #include <node_buffer.h>
 #include <unistd.h>

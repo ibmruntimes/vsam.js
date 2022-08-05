ef7e53361dc19c73186d4347e162c777d3e019d9
Gaby Baghdadi <baghdadi@ca.ibm.com>
2022-08-05 16:30:15 -0400

    update copyright year

--- VsamFile.h
+++ VsamFile.h
@@ -1,6 +1,6 @@
 /*
  * Licensed Materials - Property of IBM
- * (C) Copyright IBM Corp. 2017, 2021. All Rights Reserved.
+ * (C) Copyright IBM Corp. 2017, 2022. All Rights Reserved.
  * US Government Users Restricted Rights - Use, duplication or disclosure
  * restricted by GSA ADP Schedule Contract with IBM Corp.
  */
